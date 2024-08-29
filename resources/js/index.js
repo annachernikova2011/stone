15de2d1774138628fdf212c7f80bf11bcd8ffe92
Анна <>
2024-08-30 02:19:24 +0300

    mobile lendig

---
+++ resources/js/index.js
@@ -0,0 +1,34 @@
+const swiperEl = document.querySelector('swiper-container');
+
+const params = {
+   injectStyles: [`
+   .swiper-pagination-current,
+   .swiper-pagination-total,
+   .custom-separator {
+      color: #FFFFFF;
+      font-family: 'TT Hoves Pro Regular',
+   }
+    .swiper-pagination-fraction {
+      background: #141416;
+      opacity: .8;
+      width: 20vw;
+      padding: 1.2vh 1.2vh;
+      border-radius: 130px;
+      position: absolute;
+      left: 50%;
+      transform: translateX(-50%);
+    }
+    `],
+  pagination: {
+    type: 'fraction',
+    renderFraction: function (currentClass, totalClass) {
+      return '<span class="' + currentClass + '"></span>' 
+      + '<span class="custom-separator"> из </span>' 
+      + '<span class="' + totalClass + '"></span>';
+  },
+  },
+}
+
+Object.assign(swiperEl, params)
+
+swiperEl.initialize();
